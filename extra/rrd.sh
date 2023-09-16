d9e39757b1c2f4900c5d423b97941e421dfaa6b8
Thomas Habets <thomas@habets.se>
2023-09-16 15:40:15 +0100

    Add extra/rrd.sh

---
+++ extra/rrd.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+
+RRD=test.rrd
+
+MIN=4
+HOUR=$((4 * 60))
+SIXM=$((60 * 24 * 180))
+if false; then
+    rrdtool create "${RRD?}" \
+	    --start "$(date +%s -d2023-01-01)" \
+	    -s 15 \
+	    DS:w:GAUGE:600:0:10000 \
+	    DS:wh:COUNTER:600:0:10000000 \
+	    RRA:MIN:0.5:${MIN?}:${SIXM?} \
+	    RRA:MAX:0.5:${MIN?}:${SIXM?} \
+	    RRA:AVERAGE:0.5:${MIN?}:${SIXM?} \
+	    RRA:MIN:0.5:${HOUR?}:${SIXM?} \
+	    RRA:MAX:0.5:${HOUR?}:${SIXM?} \
+	    RRA:AVERAGE:0.5:${HOUR?}:${SIXM?}
+fi
+
+if false; then
+    sed 's/,/ /g' t.csv | while read T NR W KWH BAT STATUS; do
+	WH=$(echo "$KWH * 1000" | bc -l | sed -e 's/[.]0*$//')
+	S="$T:$W:$WH"
+	#echo $S
+	rrdtool update "${RRD?}" "${S?}" || true
+    done
+fi
+	
+exec rrdtool graph test.png \
+	-z \
+	-g \
+	-X 0 \
+	-y '50:2' \
+	-t 'Power consumption in watts' \
+	-l 0 \
+	-w 1280 \
+	-h 720 \
+	-s -7d \
+	-e -1h \
+	-a PNG \
+	'DEF:bleh=test.rrd:w:AVERAGE:step=600:reduce=MIN' \
+	'DEF:wh=test.rrd:wh:MIN' \
+	'LINE2:bleh#000000:Watts'
