24af1d2cd93ba22dc0f0fdb931552454fdf9dd2d
Thomas Habets <thomas@habets.se>
2024-10-11 21:37:14 +0100

    Fix rrd script

--- extra/rrd.sh
+++ extra/rrd.sh
@@ -7,7 +7,7 @@ RRD="test.rrd"
 MIN=4
 HOUR=$((4 * 60))
 SIXM=$((60 * 24 * 180))
-if false; then
+if [ ! -f "${RRD?}" ]; then
     echo "Creating rrd file…"
     rrdtool create "${RRD?}" \
 	    --start "$(date +%s -d2023-01-01)" \
@@ -25,29 +25,34 @@ fi
 if true; then
     LAST="$(rrdtool last "${RRD?}")"
     echo "Adding data…"
-    awk '
-BEGIN {
-  FS=","
-  OFS=":"
-}
-$1 > '"${LAST?}"' {
-  print $1, $3, $4 * 1000
-}
-' t.csv \
-	| xargs rrdtool update "${RRD?}"
+    grep ',OK$' t.csv \
+    | python3 <(cat <<EOF
+import sys
+for line in sys.stdin:
+    e = line.split(',')
+    if int(e[0]) > ${LAST?}:
+      print('%s:%s:%d' % (e[0], e[2], int(float(e[3])*1000)))
+EOF
+) | tee awked.txt \
+	| xargs -r rrdtool update "${RRD?}"
 fi
 echo "Graphing…"
-exec rrdtool graph test.png \
-	-g \
-	-X 0 \
-	-y '50:2' \
-	-t 'Power consumption in watts' \
-	-l 0 \
-	-w 1280 \
-	-h 720 \
-	-s -7d \
-	-e -1h \
-	-a PNG \
-	'DEF:bleh=test.rrd:w:AVERAGE:step=600:reduce=MIN' \
-	'DEF:wh=test.rrd:wh:MIN' \
-	'LINE2:bleh#000000:Watts'
+for span in 1d 7d 180d 1y 2y; do
+        rrdtool graph $span.png \
+                -g \
+                -X 0 \
+                -y '50:2' \
+                -t 'Power consumption in watts' \
+                -l 0 \
+                -w 1280 \
+                -h 720 \
+                -s -$span \
+                -e -1h \
+                -a PNG \
+                'DEF:min=test.rrd:w:AVERAGE:step=600:reduce=MIN' \
+                'DEF:avg=test.rrd:w:AVERAGE:step=600:reduce=AVERAGE' \
+                'DEF:max=test.rrd:w:AVERAGE:step=600:reduce=MAX' \
+                'DEF:wh=test.rrd:wh:MIN' \
+                'LINE2:min#80ff80:Watts' \
+                'LINE2:avg#000000:Watts'
+done
